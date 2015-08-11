632d1429c4d26fa33843df85bc2c5edf0c9f6aa9
Yancey Thomas <ythomas@leaders6-12.org>
2015-08-11 15:22:08 +0000

    decimals on calculator

--- calculator.rb
+++ calculator.rb
@@ -1,40 +1,57 @@
 def addition 
   puts "Type the first number that you want to add and press enter"
-  number = gets.chomp.to_i 
+  number = gets.chomp.to_f
   puts "Type the second number you want to add and press enter"
-  number2 = gets.chomp.to_i
+  number2 = gets.chomp.to_f
+   puts "This is the sum of your problem."
   puts number + number2  
-  puts "This is the sum of your problem."
+ 
 end 
 
 
 def subtraction 
   puts "Type the first number that you want to subtract and press enter"
-  number = gets.chomp.to_i 
+  number = gets.chomp.to_f 
   puts "Type the second number you want to subtract and press enter"
-  number2 = gets.chomp.to_i 
-  puts number - number2  
+  number2 = gets.chomp.to_f
   puts "This is the difference of your problem."
+  puts number - number2  
+  
 end 
 
 
 def multiplication 
 puts "Type the first number that you want to multiply and press enter"
-number = gets.chomp.to_i 
+number = gets.chomp.to_f
 puts "Type the second number you want to multiply and press enter"
-number2 = gets.chomp.to_i 
-puts number * number2  
+number2 = gets.chomp.to_f
   puts "This is the product of your problem."
+puts number * number2  
+  
+end 
+
+def division 
+  puts "Type the first number that you want to divide and press enter"
+number = gets.chomp.to_f
+  puts "Type the second number you want to divide and press enter"
+number2 = gets.chomp.to_f 
+ puts "This is the quotient of your problem."
+puts number / number2  
+  
 end 
 
 
 
-puts "Welcome to Good Burger what operation yall want?"
+puts "Hello there! Which type of operation would you like to do today?"
 operation = gets.chomp
-if operation == "+"
+if operation == "addition"
   puts addition
-elsif operation == "-"
+elsif operation == "subtraction"
   puts subtraction
+elsif operation == "multiplication"
+  puts multiplication
+elsif operation == "division"
+  puts division
   end
 
   
